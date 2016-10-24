326382986f8292c2838715a6a1d611e92cb5778b
Bartłomiej Szałach <onegrx@gmail.com>
2016-10-24 22:55:38 +0200

    Simulate fetching data in background

--- to-javafx-4/src/main/java/pl/edu/agh/iisg/to/javafx/cw4/controller/AccountOverviewController.java
+++ to-javafx-4/src/main/java/pl/edu/agh/iisg/to/javafx/cw4/controller/AccountOverviewController.java
@@ -3,7 +3,9 @@ package pl.edu.agh.iisg.to.javafx.cw4.controller;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javafx.application.*;
 import javafx.beans.binding.Bindings;
+import javafx.concurrent.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,8 +16,69 @@ import javafx.scene.control.TableView;
 import pl.edu.agh.iisg.to.javafx.cw4.model.Account;
 import pl.edu.agh.iisg.to.javafx.cw4.model.Category;
 import pl.edu.agh.iisg.to.javafx.cw4.model.Transaction;
+import pl.edu.agh.iisg.to.javafx.cw4.model.generator.*;
 
 public class AccountOverviewController {
+
+    private static class TransactionGeneratorService extends Service<Void> {
+
+        private final Account account;
+
+        public TransactionGeneratorService(Account account) {
+            this.account = account;
+        }
+
+        @Override
+        protected Task<Void> createTask() {
+
+            return new Task<Void>() {
+
+                @Override
+                protected Void call() throws Exception {
+                    int iterations;
+
+                    final int maxIterations = 20;
+                    for (iterations = 0; iterations < maxIterations; iterations++) {
+                        if (isCancelled()) {
+                            updateMessage("Cancelled");
+                            break;
+                        }
+                        updateMessage("Iteration " + iterations);
+                        updateProgress(iterations + 1, maxIterations);
+
+                        final Transaction t = doHardStuff();
+                        if (t != null) {
+                            // runs the method in UI thread
+                            Platform.runLater(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    account.addTransaction(t);
+
+                                }
+
+                            });
+                        }
+                    }
+                    return null;
+                }
+
+                private Transaction doHardStuff() {
+                    try {
+                        Thread.sleep(100);
+                        return DataGenerator.generateTransaction();
+                    } catch (InterruptedException interrupted) {
+                        if (isCancelled()) {
+                            updateMessage("Cancelled");
+                        }
+                        return null;
+                    }
+
+                }
+            };
+        }
+
+    }
 
 	private Account data;
 
@@ -87,7 +150,14 @@ public class AccountOverviewController {
 			data.addTransaction(transaction);
 		}
 	}
-	
+
+	@FXML
+	private void handleLoadData(ActionEvent event) {
+		TransactionGeneratorService service = new TransactionGeneratorService(data);
+		progressBar.progressProperty().bind(service.progressProperty());
+		progressBar.visibleProperty().bind(service.runningProperty());
+		service.start();
+	}
 
 	public void setData(Account acccount) {
 		this.data = acccount;
